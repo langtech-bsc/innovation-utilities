8209f8ac868d2cf27cff1cfa2fe2ca345e2bc693
Marti Umbert <marti.umbert@bsc.es>
2025-05-06 11:29:26 +0200

    scripts/speech/run_transcribe_and_evaluate_folder_example_renfe380.sh: bash script to launch example

---
+++ scripts/speech/run_transcribe_and_evaluate_folder_example_renfe380.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# DEFAULT PARAMETERS
+SCRIPT_DIR=$(dirname "$0")
+DEFAULT_PYTHON_SCRIPT="$SCRIPT_DIR/../../examples/speech/transcribe_and_evaluate_folder_example.py"
+DEFAULT_YAML_CONFIG="$SCRIPT_DIR/../../config/speech/transcribe_and_evaluate_folder_renfe380.yaml"
+
+# PARAMETERS
+PYTHON_SCRIPT=$(realpath ${1:-$DEFAULT_PYTHON_SCRIPT})
+YAML_CONFIG=$(realpath ${2:-$DEFAULT_YAML_CONFIG})
+
+# VALIDATE INPUTS
+[ ! -f "$PYTHON_SCRIPT" ] && { echo "Python script not found: $PYTHON_SCRIPT"; exit 1; }
+[ ! -f "$YAML_CONFIG" ] && { echo "Python YAML file not found: $YAML_CONFIG"; exit 1; }
+
+cmd="time poetry run python $PYTHON_SCRIPT --yaml_config $YAML_CONFIG"
+echo $cmd && eval $cmd
