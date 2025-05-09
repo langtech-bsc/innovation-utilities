f28f0ec1f79249e7a8aa89983b031f4202a72f6a
Marti Umbert <marti.umbert@bsc.es>
2025-05-09 15:04:36 +0200

    examples/speech/transcribe_and_evaluate_folder_example.py: control workflow with do_step flag

--- examples/speech/transcribe_and_evaluate_folder_example.py
+++ examples/speech/transcribe_and_evaluate_folder_example.py
@@ -14,20 +14,31 @@ def main(yaml_config: str = None):
     io_utils.show_config(config)
 
     # List files to process
-    data = collect_data_module.collect_data(config = config["modules"]["collect_data"])
-    data()
+    if config["modules"]["collect_data"]["do_step"]:
+        data = collect_data_module.collect_data(config = config["modules"]["collect_data"])
+        data()
+    else:
+        print("Skipping data collection step.")
 
     # Apply ASR to the listed files
-    asr = transcribe_audios_module.asr_factory(config = config["modules"]["transcribe_audios"])
-    asr(wav_files = data.files)
+    if config["modules"]["transcribe_audios"]["do_step"]:
+        asr = transcribe_audios_module.asr_factory(config = config["modules"]["transcribe_audios"])
+        asr(wav_files = data.files)
+    else:
+        print("Skipping audio transcription step.")
 
     # Evaluate the transcripts
     evaluate = transcripts_module.evaluate_transcriptions(config = config["modules"]["evaluate_transcriptions"])
-    evaluate(transcription_files = asr.all_output_files,
-             reference_folder = config["modules"]["evaluate_transcriptions"]["params"]["reference_folder"])
-    # evaluate(transcription_files = ["data/speech/renfe380/transcriptions/199a60e8-159b-44ca-95a4-36f2825a1de6.json",
-    #                                 "data/speech/renfe380/transcriptions/d629e7cc-1a18-4f51-971a-4d5b1bbf38c9.json"],
-    #         reference_folder = config["modules"]["evaluate_transcriptions"]["params"]["reference_folder"])
+    if config["modules"]["evaluate_transcriptions"]["do_step"]:
+        if config["modules"]["transcribe_audios"]["do_step"]:
+            evaluate(transcription_files = asr.all_output_files,
+                     reference_folder = config["modules"]["evaluate_transcriptions"]["params"]["reference_folder"])
+        else:
+            evaluate(transcription_folder = config["modules"]["transcribe_audios"]["data"]["output_folder"],
+                     transcription_extension = config["modules"]["transcribe_audios"]["data"]["output_extensions"][0],
+                     reference_folder = config["modules"]["evaluate_transcriptions"]["params"]["reference_folder"])
+    else:
+        print("Skipping evaluation step.")
 
 if __name__ == '__main__':
 
